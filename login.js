e49557aa14f65b652eae8f57ca020a000dc5a913
martingathu <martin5gathu@gmail.com>
2020-01-30 18:43:21 +0300

    edited pages

--- login.js
+++ login.js
@@ -3,19 +3,33 @@ function check(form)
 
 if(form.userid.value == "martingathu" && form.pwd.value === "auntyalice")
 {
-	return true;
+	alert ("Login successfully");
+	window.location = "userhomepage.html"; // Redirecting to users home page.
+	return false;
+	
 }
 else if(form.userid.value == "Joel-359" && form.pwd.value === "otieno3")
 {
-	return true;
+	window.location = "userhomepage.html"; // Redirecting to users home page.
+	return false;
 }
 else if(form.userid.value == "shalomodida" && form.pwd.value === "Muthoni.12")
 {
-	return true;
+	window.location = "userhomepage.html"; // Redirecting to users users page.
+	return false;
+
 }
 else if(form.userid.value == "joan-w" && form.pwd.value === "JKinyua")
 {
-	return true;
+	window.location = "userhomepage.html"; // Redirecting to users home page.
+	return false;
+	
+}
+else if(form.userid.value == "admin" && form.pwd.value === "admin")
+{
+	window.location = "Admin.html"; // Redirecting to users admin page.
+	return false;
+
 }
 else
 {
